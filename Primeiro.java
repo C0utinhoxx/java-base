9846d8ee81e63c16453f0693e5073afe2f153d45
Bruno <brunoriquelme.coutinho@gmail.com>
2024-07-20 13:02:35 -0300

    Finalizando primeira aula

---
+++ Primeiro.java
@@ -0,0 +1,17 @@
+
+//Clase em Java
+public class Primeiro{
+/*
+	** Comentário em Bloco
+	 1 - Os arquivos em java devem ser criados sempre com o mesmo nome de suas classes.
+	 2 - Os nomes desses arquivos devem sempre iniciar com a letra maiùscula utilizando o sistema PascalCase. O PasCase sempre inicia a primeira palavra com a letra em cauxa alta e as demias em caixa baixa, diferentemente do camelCase que inicia a palavra com a primeira letra e, caixa baixa e as demais em caixa baixa ou em caixaAlta
+*/
+
+
+	public static void main(String args[]){
+		
+		System.out.print("Olá Mundo!");
+	
+	}
+
+}
